afa64c7dffd23bceee313c6a4c2f0eede8a5c365
Barry <barrytan.sg@gmail.com>
2025-12-28 04:58:51 +0000

    update

--- 1-Hello-World/index.js
+++ 1-Hello-World/index.js
@@ -1,1 +1,9 @@
+let url = "https://4geeksacademy.github.io/exercise-assets/txt/hello.txt";
 
+async function  main() {
+    let response = await axios.get(url);
+    console.log(response.data);
+    let body = document.querySelector("#content");
+    body.innerHTML = `<h1>${response.data}</h1>`;
+}
+main();
